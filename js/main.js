ae6e9cbc068f4be898444507e15b2d7165fc0efc
이소윤 <YOON@isoyun-ui-MacBook-Pro.local>
2019-06-07 15:37:49 -0400

    To ask about footer html problem

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(() => {
+	console.log("Lee,Lin,Guzman midterm");
+
+
+function viewText() { 
+  let soyoon = document.getElementById(".text"); 
+  if(soyoon.offsetHeight < 100) { 
+    document.getElementById(".text").style.height = ""; 
+    text01.value = "Read More"; 
+    } else { 
+    document.getElementById(".text").style.height = "100px"; 
+    text01.value = "Read Less"; 
+ 	} 
+
+ 	
+} 
+
+
+
+
+
+})();
